bf2c8b969932e255fc2df804942b24f2191d400c
Erik Drobne <erik.drobne@gmail.com>
2023-05-25 17:27:36 +0200

    Put mocks into a separate folder.

---
+++ Tests/SwiftUICoordinatorTests/Mocks/MockRoute.swift
@@ -0,0 +1,35 @@
+//
+//  MockRoute.swift
+//  
+//
+//  Created by Erik Drobne on 25/05/2023.
+//
+
+import Foundation
+@testable import SwiftUICoordinator
+
+enum MockRoute: NavigationRoute {
+    case circle
+    case rectangle
+    case square
+    
+    var title: String? {
+        switch self {
+        case .circle:
+            return "Circle"
+        case .rectangle:
+            return "Rectangle"
+        case .square:
+            return "Square"
+        }
+    }
+
+    var action: TransitionAction? {
+        switch self {
+        case .square:
+            return nil
+        default:
+            return .push(animated: true)
+        }
+    }
+}
---
+++ Tests/SwiftUICoordinatorTests/Mocks/MockView.swift
@@ -0,0 +1,14 @@
+//
+//  MockView.swift
+//  
+//
+//  Created by Erik Drobne on 25/05/2023.
+//
+
+import SwiftUI
+
+struct MockView: View {
+    var body: some View {
+        Text("Mock View")
+    }
+}
